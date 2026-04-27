e6d8e17ded1e530d5b35bf00f61ee37b7c3a07ab
Ben.Kimball <Ben.Kimball@LLabmik.net>
2026-04-27 16:52:31 -0500

    migrations: add OrganizationId column to Calendars table

---
+++ src/Modules/Calendar/DotNetCloud.Modules.Calendar.Data/Migrations/20260427214709_AddCalendarOrganizationId.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DotNetCloud.Modules.Calendar.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCalendarOrganizationId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "OrganizationId",
+                table: "Calendars",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "ix_calendars_organization_id",
+                table: "Calendars",
+                column: "OrganizationId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "ix_calendars_organization_id",
+                table: "Calendars");
+
+            migrationBuilder.DropColumn(
+                name: "OrganizationId",
+                table: "Calendars");
+        }
+    }
+}
