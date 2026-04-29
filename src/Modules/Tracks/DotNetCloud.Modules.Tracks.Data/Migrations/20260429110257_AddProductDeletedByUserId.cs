acf2af5a6f539bf3a61e841d727c0b4306c16aa4
Ben.Kimball <Ben.Kimball@LLabmik.net>
2026-04-29 06:09:12 -0500

    Add EF migration for Product.DeletedByUserId column

---
+++ src/Modules/Tracks/DotNetCloud.Modules.Tracks.Data/Migrations/20260429110257_AddProductDeletedByUserId.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DotNetCloud.Modules.Tracks.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProductDeletedByUserId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "DeletedByUserId",
+                table: "Products",
+                type: "uuid",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DeletedByUserId",
+                table: "Products");
+        }
+    }
+}
